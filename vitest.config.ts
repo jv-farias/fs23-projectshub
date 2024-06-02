3b46ad290d89d8e9b8d668328a31107bdb4ccbec
AbraaoAlves <abraao.teodosio@gmail.com>
2024-06-02 22:34:22 +0000

    config: avoid import jest-dom in all tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    setupFiles: "src/setupTests.ts",
     globals: true,
     typecheck: {
       enabled: false // speedup tests
